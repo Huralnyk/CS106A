396e4e9ead389866d86b3d9d53a4dbf5228b7410
Huralnyk <alexey.huralnyk@gmail.com>
2015-10-25 21:53:06 +0200

    Processing

--- Assignment4/Hangman.java
+++ Assignment4/Hangman.java
@@ -1,8 +1,9 @@
 /*
  * File: Hangman.java
  * ------------------
- * This program will eventually play the Hangman game from
- * Assignment #4.
+ * Author: Alexey Huralnyk
+ * This program plays the Hangman game from 
+ * Assignment #4 (Stanford's CS106A course).
  */
 
 import acm.graphics.*;
@@ -12,9 +13,38 @@ import acm.util.*;
 import java.awt.*;
 
 public class Hangman extends ConsoleProgram {
+	
+	/** Private constants */
+	private static final int N_GUESSES = 8;
 
     public void run() {
-		/* You fill this in */
+		startNewGame();
+		println("Welcome to Hangman");
+		println("The word now looks like this: " + currentWord);
+		
+		
 	}
-
+    
+    private void startNewGame() {
+    	int index = rgen.nextInt(lexicon.getWordCount());
+    	secret = lexicon.getWord(index);
+    	currentWord = dashify(secret);
+    	guessesLeft = N_GUESSES;
+    }
+    
+    private String dashify(String str) {
+    	String result = "";
+    	for (int i = 0; i < str.length(); i++) {
+    		result += "—";
+    	}
+    	return result;
+    } 
+    
+    /** Private instance variables */
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private HangmanLexicon lexicon = new HangmanLexicon();
+    
+    private String secret;
+    private String currentWord;
+    private int guessesLeft;
 }
