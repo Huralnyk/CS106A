b45f438316714a19fd3e8ac824290e41607d144e
Huralnyk <alexey.huralnyk@gmail.com>
2015-11-01 20:32:46 +0200

    Assignment 4

--- Assignment4/HangmanLexicon.java
+++ Assignment4/HangmanLexicon.java
@@ -1,33 +1,49 @@
 /*
  * File: HangmanLexicon.java
  * -------------------------
- * This file contains a stub implementation of the HangmanLexicon
- * class that you will reimplement for Part III of the assignment.
+ * This file contains a implementation of the HangmanLexicon which
+ * provide lexicon to Hangman game from a file specified by constant
+ * FILENAME.
  */
 
 import acm.util.*;
+import java.io.*;
+import java.util.*;
 
 public class HangmanLexicon {
+	
+	/** Private constants */
+	private static final String FILENAME = "ShorterLexicon.txt";
+	
+	/**
+	 * This is the HangmanLexicon constructor.
+	 * Initializes content of lexicon from a file specified
+	 * by a constant FILENAME.
+	 */
+	public HangmanLexicon() {
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader(FILENAME));
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) break;
+				words.add(line);
+			}
+			rd.close();
+		} catch (IOException ex){
+			throw new ErrorException("File not found");
+		}
+	}
 
 /** Returns the number of words in the lexicon. */
 	public int getWordCount() {
-		return 10;
+		return words.size();
 	}
 
 /** Returns the word at the specified index. */
 	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
+		return words.get(index);
 	};
+	
+	/** Private instance variables */
+	private ArrayList<String> words = new ArrayList<String>();
 }
